ee5e5dc2322f377d22035df6bdeb88d228c9fb69
antonynans <antonynans@gmail.com>
2021-01-16 00:44:12 +0100

    created the login file

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {NotificationManager} from '@react-notifications';
 import {cometchat} from '@cometchat-pro/chat'
 import config from '../config';
@@ -6,6 +6,23 @@ import config from '../config';
 const Login = (props) => {
     const [uidValue, setUidValue] = useState('');
     const [isSubmitting, setisSubmitting] = useState(false);
+
+    const handleSubmit = eent => {
+        event.preventDefault();
+        setisSubmitting(true);
+        CometChat.login(uidValue, config.apiKey).then(
+            User => {
+                NotificationManager.success('You are now logged in', 'Login Success');
+                console.log('Login Successful:', {User});
+                props.setUser(User);
+            },
+            error => {
+                NotificationManager.error('Please try again', 'Login failed');
+                const.log('Login failedwith exception:', {error});
+                setisSubmitting(false);
+            }
+        );
+    };
 
     return (
         ### Login to Awesome Chat
@@ -25,7 +42,7 @@ const Login = (props) => {
         value={`${isSubmitting ? 'Loading...' : 'Login'}`}
         disabled={isSubmitting}
         />
-        
+
     )
 }
 
